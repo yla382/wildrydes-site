9dc7e7051161737068312a35241ba8c73a761198
yla382 <yoonhong_lee@sfu.ca>
2020-11-15 16:05:51 -0800

    config updated

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_anPy58r6p', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '467u1r68o00r5kl05nga1m96k5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
